2047fd9ccde39730f59c82ffa8f4b18c1d29e10c
Mostafa <nonatefa11@gmail.com>
2023-04-16 19:30:43 +0200

    create static file and const the image

---
+++ lib/core/constant/imagesassets.dart
@@ -0,0 +1,6 @@
+class ImageAssets {
+  static const String OnBoradingImageOne = "assets/images/onboardingone.png";
+  static const String OnBoradingImageTwo = "assets/images/onboardingtwo.png";
+static const String OnBoradingImageThree ="assets/images/onboardingthree.png";
+  static const String OnBoradingImageFour = "assets/images/onboardingfour.png";
+}
---
+++ lib/data/datasource/Static/static.dart
@@ -0,0 +1,25 @@
+import 'package:ecommerce/core/constant/imagesassets.dart';
+import 'package:ecommerce/data/model/onboardingmodel.dart';
+
+List<OnBoardingModel> OnBoardingList = [
+  OnBoardingModel(
+      title: "Choose Product",
+      body:
+          "We have a 100k Products. Choose \n your Product from our E-commerce Shop",
+      image: ImageAssets.OnBoradingImageOne),
+  OnBoardingModel(
+      title: "Easy & Safe Payment",
+      body:
+          "We have a 100k Products. Choose \n your Product from our E-commerce Shop",
+      image: ImageAssets.OnBoradingImageTwo),
+  OnBoardingModel(
+      title: "Track your Order",
+      body:
+          "We have a 100k Products. Choose \n your Product from our E-commerce Shop",
+      image: ImageAssets.OnBoradingImageThree),
+  OnBoardingModel(
+      title: "Fast Delivery",
+      body:
+          "We have a 100k Products. Choose \n your Product from our E-commerce Shop",
+      image: ImageAssets.OnBoradingImageFour)
+];
